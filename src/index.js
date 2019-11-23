fcd7ec3c2bdb31f846ccb44ff330dd523798059d
homer0 <leo.apiwan@olapic.com>
2019-11-23 04:55:52 -0300

    feat: add library base code

---
+++ src/index.js
@@ -0,0 +1,92 @@
+const { toString } = Object.prototype;
+const { toString: fnToString } = Function.prototype;
+const reBase = String(toString)
+.replace(/[.*+?^${}()|[\]\/\\]/g, '\\$&')
+.replace(/toString|(function).*?(?=\\\()| for .+?(?=\\\])/g, '$1.*?');
+const reNative = RegExp(`^${reBase}$`);
+
+const isNativeFn = (fn) => fnToString.call(fn).match(reNative);
+
+const getInjectData = (target = [], enhancement = []) => {
+  const list = target.slice();
+  const enhancementPositions = {};
+  enhancement.forEach((dep) => {
+    const targetIndex = list.indexOf(dep);
+    if (targetIndex > -1) {
+      enhancementPositions[dep] = targetIndex;
+    } else {
+      list.push(dep);
+      enhancementPositions[dep] = list.length - 1;
+    }
+  });
+
+  return {
+    list,
+    getForTarget: (values) => values.slice(0, target.length),
+    getForEnhancement: (values) => enhancement.map((dep) => (
+      values[enhancementPositions[dep]]
+    )),
+  };
+};
+
+const composeFn = (target, enhancement) => (...args) => {
+  const enhanced = enhancement(...args);
+  return enhanced && typeof enhanced.then === 'function' ?
+    enhanced.then((value) => target(value, ...args)) :
+    target(...args);
+};
+
+const enhanceInstance = (target, enhancement) => new Proxy(target, {
+  get: (targetCls, name) => {
+    let result;
+    if (
+      typeof targetCls[name] === 'function' &&
+      !isNativeFn(targetCls[name]) &&
+      typeof enhancement[name] === 'function'
+    ) {
+      result = composeFn(targetCls[name], enhancement[name]);
+    } else {
+      result = targetCls[name];
+    }
+
+    return result;
+  },
+});
+
+const composeWith = (Target, Enhancement) => {
+  const injectData = getInjectData(Target.inject, Enhancement.inject);
+  return new Proxy(Target, {
+    construct: (TargetCls, args) => {
+      const targetInstance = new TargetCls(...injectData.getForTarget(args));
+      const enhancementInstance = new Enhancement(
+        targetInstance,
+        ...injectData.getForEnhacement(args)
+      );
+
+      return enhanceInstance(targetInstance, enhancementInstance);
+    },
+    get: (target, name) => (
+      name === 'inject' ?
+        injectData.list :
+        target[name]
+    ),
+  });
+};
+
+const composeViewModel = (Target, ...enhancements) => enhancements.reduce(
+  (Current, Enhancement) => composeWith(Current, Enhancement),
+  Target
+);
+
+const compose = (...enhancements) => (Target) => composeViewModel(
+  Target,
+  ...enhancements
+);
+
+module.exports = new Proxy(compose, {
+  get: (target, name) => (
+    name === 'composeViewModel' ?
+      composeViewModel :
+      target[name]
+  ),
+});
